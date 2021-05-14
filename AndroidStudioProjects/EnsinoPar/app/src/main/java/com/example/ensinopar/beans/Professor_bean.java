acca54668aa0cb5a7e12146227260dc14d649591
Guilherme Hachimine <gui.hachimine@gmail.com>
2021-05-14 09:54:36 -0300

    removendo Ensino Par

--- AndroidStudioProjects/EnsinoPar/app/src/main/java/com/example/ensinopar/beans/Professor_bean.java
+++
@@ -1,8 +0,0 @@
-package com.example.ensinopar.beans;
-
-public class Professor_bean {
-    private String MatriculaProf;
-    private String nome;
-    private String telefone;
-    private String email;
-}
